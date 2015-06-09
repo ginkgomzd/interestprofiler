edd7df229a131dc68d7f10039e3ea793e7d3a7a9
Frank J. Gómez <frank@ginkgostreet.com>
2015-06-09 18:47:56 -0400

    Set default router to load question data into temporary storage on app start.

--- ember-profiler/app/models/question.js
+++ ember-profiler/app/models/question.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  index: DS.attr('number'),
+  index: DS.attr('number'), // not sure we need this; Ember reserves the ID so we're using this instead of an ID
   area: DS.attr('string'),
   text: DS.attr('string')
 });
