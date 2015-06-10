26fe10eba8ac946b8fed5a737cda288e5ad349ee
ginkgoMZD <mzd@ginkgostreet.com>
2015-06-10 12:01:50 -0400

    scaffold and mock for results model;

--- ember-profiler/app/routes/results.js
+++ ember-profiler/app/routes/results.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model: function() {
+      var answers = getAnswers();
+
+    }
 });
+
+function getAnswers() {
+  return '111111111111111111111111111111111111111111111111111111111111';
+}
