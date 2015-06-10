becc6606e0f4616cce1ffcec6b063257dc6dc211
Frank J. Gómez <frank@ginkgostreet.com>
2015-06-09 20:32:13 -0400

    Set up route for individual questions. Passed array of question option models to question views.

--- ember-profiler/app/router.js
+++ ember-profiler/app/router.js
@@ -6,6 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('question', {path: '/question/:index'});
 });
 
 export default Router;
--- ember-profiler/app/routes/question.js
+++ ember-profiler/app/routes/question.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(params) {
+    return this.store.find('question', params.index);
+  },
+
+  setupController: function(controller, model) {
+    controller.set('question', model);
+    controller.set('questionOptions', this.store.all('questionOption'));
+  }
 });
