395dc9b546b5393e0705eb13ae5e47962efccc9c
ginkgoMZD <mzd@ginkgostreet.com>
2015-06-11 11:14:59 -0400

    add field cluster.selected

--- ember-profiler/app/models/cluster.js
+++ ember-profiler/app/models/cluster.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   pathways: DS.hasMany('pathway'),
-  occupations: DS.hasMany('occupation')
+  occupations: DS.hasMany('occupation'),
+  is_selected: DS.attr('boolean')
 });
