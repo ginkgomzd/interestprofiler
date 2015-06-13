d7f3d9ecabb99b1d4ee06770fa947536d8256607
ginkgoMZD <mzd@ginkgostreet.com>
2015-06-13 17:02:24 -0400

    change model field name

--- ember-profiler/app/models/pathway.js
+++ ember-profiler/app/models/pathway.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  pathway: DS.attr('string'),
+  name: DS.attr('string'),
   clusterId: DS.belongsTo('cluster'),
   occupations: DS.hasMany('occupation')
 });
