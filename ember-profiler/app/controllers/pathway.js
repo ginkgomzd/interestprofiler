8095d96d7aae3b2bbd79139f98352713b2ecab15
ginkgoMZD <mzd@ginkgostreet.com>
2015-06-15 11:42:43 -0400

    add occupations and other info to pathway item display;

---
+++ ember-profiler/app/controllers/pathway.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  store: Ember.inject.service('store'),
+  allOccupations: function() {
+    return this.get('store').all('occupation');
+  },
+  occupations: function() {
+    var occupations = this.allOccupations();
+    var pathwayId = this.get('model').get('id');
+    return occupations.filter(function(occup){
+          return (occup.get('pathwayId').get('id') == pathwayId)
+        });
+  }.property('allOccupations'),
+  cluster: function() {
+    var clusterId = this.get('model').get('clusterId');
+    var cluster = this.get('store').find('cluster', clusterId.get('id'))
+    console.dir(cluster);
+    return cluster;
+  }.property()
+});
