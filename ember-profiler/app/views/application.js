5c6473202f50a2ddd7a92576270cc080b24b90c5
Toby Lounsbury <toby@ginkgostreet.com>
2015-07-06 16:58:29 -0700

    Removed debugging lines

--- ember-profiler/app/views/application.js
+++ ember-profiler/app/views/application.js
@@ -2,12 +2,10 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
   tagName: 'div',
-  classNames: ["Toby"],
   templateName: 'application',
   panRight: function(e) {
     if (e.originalEvent.gesture.deltaX > 100) {
       if(e.originalEvent.gesture.pointers[0].pageX < 130) {
-        console.log("OpenDrawer");
         this.get("controller").send("showDrawer");
         return e.preventDefault();
       }
