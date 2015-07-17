9d87b8a2fbb6e548afee6dc6c9bf2ece1cf4b615
Toby Lounsbury <toby@ginkgostreet.com>
2015-07-16 20:53:20 -0700

    Added login hide/show mechanism for titlebar, menu and progress bar

--- ember-profiler/app/views/application.js
+++ ember-profiler/app/views/application.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
   tagName: 'div',
+  classNameBindings: ["parseAuth.loggedIn:menu-shown:menu-hidden"],
   templateName: 'application',
   panRight: function(e) {
     if (e.originalEvent.gesture.deltaX > 100) {
