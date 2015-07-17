fbc9de726b256eaa5c27cd369300b2b214730379
Toby Lounsbury <toby@ginkgostreet.com>
2015-07-16 20:56:16 -0700

    Added service links and actions to login screen

--- ember-profiler/app/routes/login.js
+++ ember-profiler/app/routes/login.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    loginWithFacebook: function() {
+      var that = this;
+      this.parseAuth.authenticate_fb(function() {
+        that.transitionTo("welcome");
+      },
+      function(user, error) {
+        console.log(error);
+      });
+
+    },
+    loginWithLinkedIn: function() {this.status.error("This hasn't been implemented yet");},
+    loginWithTwitter: function() {this.status.error("This hasn't been implemented yet");},
+    loginWithGoogle: function() {this.status.error("This hasn't been implemented yet");}
+  }
 });
