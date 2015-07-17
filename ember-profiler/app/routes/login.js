39973f4f29a3d28d9802d9f07b4d8efc4b5ec6a0
Toby Lounsbury <toby@ginkgostreet.com>
2015-07-17 08:28:07 -0700

    Added email based login and signup

--- ember-profiler/app/routes/login.js
+++ ember-profiler/app/routes/login.js
@@ -13,12 +13,57 @@ export default Ember.Route.extend({
         that.transitionTo("welcome");
       },
       function(user, error) {
-        console.log(error);
+        that.status.warn(error.message);
       });
 
     },
-    loginWithLinkedIn: function() {this.status.error("This hasn't been implemented yet");},
-    loginWithTwitter: function() {this.status.error("This hasn't been implemented yet");},
-    loginWithGoogle: function() {this.status.error("This hasn't been implemented yet");}
+    loginWithLinkedIn: function() {this.status.warn("This hasn't been implemented yet");},
+    loginWithTwitter: function() {this.status.warn("This hasn't been implemented yet");},
+    loginWithGoogle: function() {this.status.warn("This hasn't been implemented yet");},
+    doLogin: function() {
+      var that = this;
+      var user = {
+        email: Ember.$("#login-email").val(),
+        password: Ember.$("#login-password").val()
+      };
+      this.parseAuth.authenticate(user,
+        function() {
+          //success
+          that.transitionTo("welcome");
+        },
+        function(user, error) {
+          that.status.warn(error.message);
+        });
+    },
+    doSignup: function() {
+      var user = {
+        email: Ember.$("#login-email").val(),
+        password: Ember.$("#login-password").val()
+      };
+      if (Ember.$("#login-password-confirm").val() === user.password) {
+        var that = this;
+        this.parseAuth.register(user,
+          function() {
+            //success
+            that.transitionTo("welcome");
+          },
+          function(user, error) {
+            that.status.warn(error.message);
+          });
+      } else {
+        this.status.warn("Passwords don't match");
+      }
+    },
+    showSignup: function() {
+      Ember.$("#login-actions").slideUp();
+      Ember.$("#signup-actions").slideDown();
+      Ember.$("#login-password-confirm").slideDown();
+      Ember.$("#login-password-confirm").css("display", "block");
+    },
+    cancelSignup: function() {
+      Ember.$("#login-actions").slideDown();
+      Ember.$("#signup-actions").slideUp();
+      Ember.$("#login-password-confirm").slideUp();
+    },
   }
 });
