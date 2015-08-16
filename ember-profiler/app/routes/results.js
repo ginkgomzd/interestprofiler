a33f2f181e48bc646787507bee904e53d2c5aed0
Toby Lounsbury <toby@ginkgostreet.com>
2015-08-16 16:26:54 -0700

    Changed dataUtils to Service rather that generic utility to take advantage of Ember's built in injection

--- ember-profiler/app/routes/results.js
+++ ember-profiler/app/routes/results.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
-import dataUtils from '../utils/data-utils';
+//import dataUtils from '../utils/data-utils';
 
 export default Ember.Route.extend({
+    dataUtils: Ember.inject.service('dataUtils'),
     model: function() {
 
       var oldAnswerString = this.settings.CalculatedAnswers;
-      var answerString = dataUtils().concatAnswerString(this.store);
+      var answerString = this.get("dataUtils").concatAnswerString(this.store);
       var scores = this.store.all('scoreArea');
 
       if (scores.get("length") === 0 ||
@@ -13,7 +14,11 @@ export default Ember.Route.extend({
         oldAnswerString !== answerString)
       {
         this.controllerFor("results").set("fetching", true);
-        dataUtils().updateAllResults(answerString, this);
+        var that = this;
+        this.get("dataUtils").updateAllResults(answerString, this.store).then(function(result) {
+          console.log("All");
+          that.controllerFor("results").set("fetching", false);
+        });
       }
 
       return this.store.find('scoreArea');
---
+++ ember-profiler/app/services/data-utils.js
@@ -0,0 +1,95 @@
+import Ember from 'ember';
+import onet from 'onet';
+
+var dataUtils = Ember.Object.extend({
+  store: Ember.inject.service('store'),
+  concatAnswerString: function (store)
+  {
+    var answerString = "";
+    store.all('answer').forEach(function (item) {
+      answerString += item.get('selection');
+    });
+    //This pads the answer string with 3's to 60 characters in length
+    return String(answerString + "333333333333333333333333333333333333333333333333333333333333").slice(0, 60);
+  },
+
+  updateProfilerResults: function (answerString) {
+    var store = this.get("store");
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      onet.interestProfiler.results(answerString).then(function (data) {
+          data.forEach(function (item) {
+
+            var r = store.getById('scoreArea', item.id);
+            if (r === null) {
+              r = store.createRecord('scoreArea', item);
+            }
+
+            r.set("area", item.area);
+            r.set("score", item.score);
+            r.set("desc", item.desc);
+            r.save();
+
+          });
+          resolve();
+        },
+        function(error) {
+          reject(error);
+        });
+    });
+  },
+  updateCareerResults: function(answerString) {
+    //This section fetches the career scores.
+    var store = this.get("store");
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      onet.interestProfiler.careers(answerString).then(function (data) {
+          data.forEach(function (item) {
+
+            //todo: verify that item._fit doesn't fall out of scope when the promise is resovled
+            store.find('occupation', {code: item.code}).then(function (record) {
+              var score;
+              switch (item._fit) {
+                case 'Good':
+                  score = 1;
+                  break;
+                case 'Great':
+                  score = 2;
+                  break;
+                case 'Best':
+                  score = 3;
+                  break;
+              }
+              console.log(score);
+              record.set("score", score);
+              record.save();
+            });
+          });
+          resolve();
+        },
+        function(error) {
+          reject(error);
+        });
+    });
+  },
+
+  updateAllResults: function(answerString, store) {
+    var that = this;
+    console.log(this.store);
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      var promises = {
+        results: that.updateProfilerResults(answerString, store),
+        careers: that.updateCareerResults(answerString, store)
+      };
+
+      Ember.RSVP.hash(promises).then(function (hash) {
+        //Success!
+        //route.settings.save("CalculatedAnswers", answerString);
+        resolve();
+      }, function (reason) {
+        //Failed to update all
+        reject(reason);
+      });
+    });
+  }
+});
+
+export default dataUtils;
