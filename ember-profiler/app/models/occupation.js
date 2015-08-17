0f640877882d29df0f7451dd72c05ec6bc3afa38
Toby Lounsbury <toby@ginkgostreet.com>
2015-08-16 18:17:31 -0700

    Added career default score of 0

--- ember-profiler/app/models/occupation.js
+++ ember-profiler/app/models/occupation.js
@@ -5,5 +5,5 @@ export default DS.Model.extend({
   pathway: DS.belongsTo('pathway'),
   cluster: DS.belongsTo('cluster'),
   name: DS.attr('string'),
-  score: DS.attr('number')
+  score: DS.attr('number', {defaultValue: 0})
 });
