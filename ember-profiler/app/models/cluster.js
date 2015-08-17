de0d39c597c19d613ca2c37a808ff65ac967a5bf
Toby Lounsbury <toby@ginkgostreet.com>
2015-08-16 21:03:50 -0700

    Added calculated score column to clusters and pathways

--- ember-profiler/app/models/cluster.js
+++ ember-profiler/app/models/cluster.js
@@ -4,5 +4,13 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   pathways: DS.hasMany('pathway'),
   occupations: DS.hasMany('occupation'),
-  is_selected: DS.attr('boolean', {defaultValue: false})
+  is_selected: DS.attr('boolean', {defaultValue: false}),
+  //todo: Should this be calculated off the pathway or the occupations?
+  //Should the cluster be tied directly to occupations at all?
+  score: function() {
+    var pathways = this.get("pathways");
+    return pathways.reduce(function(previousValue, pathway){
+      return previousValue + pathway.get("score");
+    }, 0);
+  }.property("pathways.@each.score")
 });
--- ember-profiler/app/models/pathway.js
+++ ember-profiler/app/models/pathway.js
@@ -4,5 +4,11 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   cluster: DS.belongsTo('cluster'),
   occupations: DS.hasMany('occupation'),
-  bookmarked: DS.attr('boolean', {defaultValue: false})
+  bookmarked: DS.attr('boolean', {defaultValue: false}),
+  score: function() {
+    var occupations = this.get("occupations");
+    return occupations.reduce(function(previousValue, occupation){
+      return previousValue + occupation.get("score");
+    }, 0);
+  }.property("occupations.@each.score")
 });
