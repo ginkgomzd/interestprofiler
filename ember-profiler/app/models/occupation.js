3df0c3f5a69e98bc37f60824f05213c76c79187a
Toby Lounsbury <toby@ginkgostreet.com>
2015-08-16 20:54:35 -0700

    Changed code from being its own column to being the id

--- ember-profiler/app/models/occupation.js
+++ ember-profiler/app/models/occupation.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  code: DS.attr('string'),
   pathway: DS.belongsTo('pathway'),
   cluster: DS.belongsTo('cluster'),
   name: DS.attr('string'),
