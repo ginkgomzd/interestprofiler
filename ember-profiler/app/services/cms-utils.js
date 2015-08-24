3484ba1631005f16b3e6d8cf9c3e4ba31036547a
Toby Lounsbury <toby@ginkgostreet.com>
2015-08-23 20:00:44 -0700

    Added basic cms utility service for pulling data from the cms

--- ember-profiler/app/services/cms-utils.js
+++ ember-profiler/app/services/cms-utils.js
@@ -1,8 +1,6 @@
 import Ember from 'ember';
 import ajax from 'ic-ajax';
 
-
-
 var cmsUtils = Ember.Object.extend({
   store: Ember.inject.service('store'),
   settings: Ember.inject.service('settings'),
@@ -14,13 +12,12 @@ var cmsUtils = Ember.Object.extend({
     }
   },
   updateAlumniContent: function(lastUpdated) {
-    var url = this.baseUrl() + "/api/alumni";
-    return new Promise(function(resolve, reject) {
+    var url = this.baseUrl() + "/api/alumni?updated=" + lastUpdated;
+    return new Ember.RSVP.Promise(function(resolve, reject) {
       ajax(url).then(function (result) {
         //This is wrapped in a promise in case we want to insert it directly
         //before resolving the promise. If we decide we don't care, and want
         //to insert elsewhere, we can return the ajax promise directly.
-        var store = this.get("store");
         resolve(result);
 
       }, function(error) {
