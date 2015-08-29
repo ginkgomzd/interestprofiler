bf68daee0f74f73f59703e1b5c4e221627d9999d
Toby Lounsbury <toby@ginkgostreet.com>
2015-08-28 22:59:39 -0700

    Updated models to factor in new data sources

--- ember-profiler/app/models/cluster.js
+++ ember-profiler/app/models/cluster.js
@@ -3,7 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   pathways: DS.hasMany('pathway'),
-  occupations: DS.hasMany('occupation'),
   is_selected: DS.attr('boolean', {defaultValue: false}),
   score: function() {
     var pathways = this.get("pathways");
@@ -16,7 +15,7 @@ export default DS.Model.extend({
     }, {squares: 0, weights: 0});
 
     if (scores.weights === 0) {return 0;}
-    
+
     return (scores.squares / scores.weights);
   }.property("pathways.@each.score")
 });
--- ember-profiler/app/models/pathway.js
+++ ember-profiler/app/models/pathway.js
@@ -4,11 +4,12 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   cluster: DS.belongsTo('cluster'),
   occupations: DS.hasMany('occupation'),
+  onetCareers: DS.hasMany('onet-career'),
   bookmarked: DS.attr('boolean', {defaultValue: false}),
   score: function() {
-    var occupations = this.get("occupations");
-    var scores =  occupations.reduce(function(previousValues, occupation){
-      var score = occupation.get("score");
+    var onetCareers = this.get("onet-career");
+    var scores =  onetCareers.reduce(function(previousValues, career){
+      var score = career.get("score");
       return {
         squares: previousValues.squares + (score * score),
         weights: previousValues.weights + score
@@ -18,5 +19,5 @@ export default DS.Model.extend({
     if (scores.weights === 0) {return 0;}
 
     return (scores.squares / scores.weights);
-  }.property("occupations.@each.score")
+  }.property("onetCareers.@each.score")
 });
