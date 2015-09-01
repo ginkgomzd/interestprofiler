1d8bf10c4c3cb893ef5e8b0e3b2676fa6743ad06
Toby Lounsbury <toby@ginkgostreet.com>
2015-09-01 16:55:12 -0700

    Updated setup to use direct db input to avoid collision with store

--- ember-profiler/app/services/setup.js
+++ ember-profiler/app/services/setup.js
@@ -13,7 +13,7 @@ import staticMasterData from '../data/master';
 
 var setupService = Ember.Object.extend({
   staticDate: "2015-08-23",
-  store: Ember.inject.service('store'),
+  //store: Ember.inject.service('store'),
   settings: Ember.inject.service('settings'),
   profilerDataUtils: Ember.inject.service('profilerDataUtils'),
   cmsUtils: Ember.inject.service('cmsUtils'),
@@ -26,167 +26,194 @@ var setupService = Ember.Object.extend({
     return this.get("cmsUtils").updateAll(lastUpdated);
   },
   staticQuestions: function() {
-    var store = this.get("store");
+    var setup = this;
     return new Ember.RSVP.Promise(function(resolve, reject) {
-      store.findAll("question").then(function(data) {
-        if (data.length >= staticMasterData.questions) {
-          staticQuestionData.forEach(function (question) {
-            var r = store.createRecord('question', question);
-            r.save();
-          });
-          resolve(true);
-        } else {
-          resolve(false);
-        }
-      });
+      if (!setup.localForageData.hasOwnProperty("question")) {
+        setup.localForageData.question = {};
+        setup.localForageData.question.records = {};
+      }
+      if (Object.keys(setup.localForageData.question.records).length < staticMasterData.questions) {
+        staticQuestionData.forEach(function (question) {
+          if(!setup.localForageData.hasOwnProperty("question") || !setup.localForageData.question.records.hasOwnProperty(question.id)) {
+            setup.localForageData.question.records[question.id] = question;
+          }
+        });
+        resolve(true);
+      } else {
+        resolve(false);
+      }
     });
   },
   staticQuestionOptions: function() {
-    var store = this.get("store");
+    var setup = this;
     return new Ember.RSVP.Promise(function(resolve, reject) {
-      store.findAll("questionOption").then(function(data) {
-        if (data.length >= staticMasterData.questionOptions) {
-          staticQuestionOptionData.forEach(function (option) {
-            var r = store.createRecord('questionOption', option);
-            r.save();
-          });
-          resolve(true);
-        } else {
-          resolve(false);
-        }
-      });
+      if (!setup.localForageData.hasOwnProperty("question-option")) {
+        setup.localForageData['question-option'] = {};
+        setup.localForageData['question-option'].records = {};
+      }
+      if (Object.keys(setup.localForageData['question-option'].records).length < staticMasterData.questionOptions) {
+        staticQuestionOptionData.forEach(function (option) {
+          if(!setup.localForageData['question-option'].records.hasOwnProperty(option.id)) {
+            setup.localForageData['question-option'].records[option.id] = option;
+          }
+        });
+        resolve(true);
+      } else {
+        resolve(false);
+      }
     });
   },
   staticClusters: function() {
-    var store = this.get("store");
+    var setup = this;
     return new Ember.RSVP.Promise(function(resolve, reject) {
-      store.findAll("cluster").then(function(data) {
-        if (data.length >= staticMasterData.clusters) {
-          staticClusterData.forEach(function (cluster) {
-            var r = store.createRecord('cluster', cluster);
-            r.save();
-          });
-          resolve(true);
-        } else {
-          resolve(false);
-        }
-      });
+      if (!setup.localForageData.hasOwnProperty("cluster")) {
+        setup.localForageData.cluster = {};
+        setup.localForageData.cluster.records = {};
+      }
+      if (Object.keys(setup.localForageData.cluster.records).length < staticMasterData.clusters) {
+        staticClusterData.forEach(function (cluster) {
+          if(!setup.localForageData.cluster.records.hasOwnProperty(cluster.id)) {
+            setup.localForageData.cluster.records[cluster.id] = cluster;
+          }
+        });
+        resolve(true);
+      } else {
+        resolve(false);
+      }
     });
   },
   staticPathways: function() {
-    var store = this.get("store");
+    var setup = this;
     return new Ember.RSVP.Promise(function(resolve, reject) {
-      store.findAll("pathway").then(function(data) {
-        if (data.length >= staticMasterData.pathways) {
-          staticPathwayData.forEach(function (pathway) {
-            pathway.cluster = store.getById("cluster", pathway.cluster);
-            var r = store.createRecord('pathway', pathway);
-            r.save();
-          });
-          resolve(true);
-        } else {
-          resolve(false);
-        }
-      });
+      if (!setup.localForageData.hasOwnProperty("pathway")) {
+        setup.localForageData.pathway = {};
+        setup.localForageData.pathway.records = {};
+      }
+      if (Object.keys(setup.localForageData.pathway.records).length < staticMasterData.pathways) {
+        staticPathwayData.forEach(function (pathway) {
+          if(!setup.localForageData.pathway.records.hasOwnProperty(pathway.id)) {
+            setup.localForageData.pathway.records[pathway.id] = pathway;
+          }
+        });
+        resolve(true);
+      } else {
+        resolve(false);
+      }
     });
   },
   staticOccupations: function() {
-    var store = this.get("store");
+    var setup = this;
     return new Ember.RSVP.Promise(function(resolve, reject) {
-      store.findAll("occupation").then(function(data) {
-        if (data.length >= staticMasterData.occupations) {
-          staticOccupationData.forEach(function (occupation) {
-            occupation.pathway = store.getById("pathway", occupation.pathway);
-            var r = store.createRecord('occupation', occupation);
-            r.save();
-          });
-          resolve(true);
-        } else {
-          resolve(false);
-        }
-      });
+      if (!setup.localForageData.hasOwnProperty("occupation")) {
+        setup.localForageData.occupation = {};
+        setup.localForageData.occupation.records = {};
+      }
+      if (Object.keys(setup.localForageData.occupation.records).length < staticMasterData.occupations) {
+        staticOccupationData.forEach(function (occupation) {
+          if(!setup.localForageData.occupation.records.hasOwnProperty(occupation.id)) {
+            setup.localForageData.occupation.records[occupation.id] = occupation;
+          }
+        });
+        resolve(true);
+      } else {
+        resolve(false);
+      }
     });
   },
   staticOnetCareers: function() {
-    var store = this.get("store");
+    var setup = this;
     return new Ember.RSVP.Promise(function(resolve, reject) {
-      store.findAll("onet-career").then(function(data) {
-        if (data.length >= staticMasterData.onetCareers) {
-          staticOnetCareerData.forEach(function (career) {
-            career.pathway = store.getById("pathway", career.pathway);
-            var r = store.createRecord('onet-career', career);
-            r.save();
-          });
-          resolve(true);
-        } else {
-          resolve(false);
-        }
-      });
+      if (!setup.localForageData.hasOwnProperty("onet-career")) {
+        setup.localForageData['onet-career'] = {};
+        setup.localForageData['onet-career'].records = {};
+      }
+      if (Object.keys(setup.localForageData['onet-career'].records).length < staticMasterData.onetCareers) {
+        staticOnetCareerData.forEach(function (career) {
+          if(!setup.localForageData['onet-career'].records.hasOwnProperty(career.id)) {
+            setup.localForageData['onet-career'].records[career.id] = career;
+          }
+        });
+        resolve(true);
+      } else {
+        resolve(false);
+      }
     });
   },
   staticAlumni: function() {
-    var store = this.get("store");
+    var setup = this;
     return new Ember.RSVP.Promise(function(resolve, reject) {
-      store.findAll("alumni").then(function(data) {
-        if (data.length >= staticMasterData.alumni) {
-          staticAlumniData.forEach(function (alumni) {
-            var r = store.createRecord('alumni', alumni);
-            r.save();
-          });
-          resolve(true);
-        } else {
-          resolve(false);
-        }
-      });
+      if (!setup.localForageData.hasOwnProperty("alumni")) {
+        setup.localForageData.alumni = {};
+        setup.localForageData.alumni.records = {};
+      }
+      if (Object.keys(setup.localForageData.alumni.records).length < staticMasterData.alumni) {
+        staticAlumniData.forEach(function (alumni) {
+          if(!setup.localForageData.alumni.records.hasOwnProperty(alumni.id)) {
+            setup.localForageData.alumni.records[alumni.id] = alumni;
+          }
+        });
+        resolve(true);
+      } else {
+        resolve(false);
+      }
     });
   },
 
   staticPrograms: function() {
-    var store = this.get("store");
+    var setup = this;
     return new Ember.RSVP.Promise(function(resolve, reject) {
-      store.findAll("program").then(function(data) {
-        if (data.length >= staticMasterData.programs) {
-          staticProgramData.forEach(function (program) {
-            var occupations = [];
-            program.occupation.forEach(function (item) {
-              occupations.push(store.getById("occupation", item));
-            });
-            program.occupation = occupations;
-            var r = store.createRecord('program', program);
-            r.save();
-          });
-          resolve(true);
-        } else {
-          resolve(false);
-        }
-      });
+      if (!setup.localForageData.hasOwnProperty("program")) {
+        setup.localForageData.program = {};
+        setup.localForageData.program.records = {};
+      }
+      if (Object.keys(setup.localForageData.program.records).length < staticMasterData.programs) {
+        staticProgramData.forEach(function (program) {
+          if(!setup.localForageData.program.records.hasOwnProperty(program.id)) {
+            setup.localForageData.program.records[program.id] = program;
+          }
+        });
+
+        resolve(true);
+      } else {
+        resolve(false);
+      }
     });
-    //this.get("store").pushMany('program', staticProgramData);
   },
 
   appStartup: function() {
     var setup = this;
     return new Ember.RSVP.Promise(function(resolve, reject) {
 
-      var staticPromises = {
-        questions: setup.staticQuestions(),
-        questionOptions: setup.staticQuestionOptions(),
-        clusters: setup.staticClusters(),
-        pathways: setup.staticPathways(),
-        careers: setup.staticOnetCareers(),
-        occupations: setup.staticOccupations(),
-        alumni: setup.staticAlumni(),
-        programs: setup.staticPrograms()
-      };
+      localforage.keys(function(err, keys) {
+        setup.localForageKey = keys[0];
+        localforage.getItem(setup.localForageKey, function(err, value) {
+          setup.localForageData = value || {};
 
-      Ember.RSVP.hash(staticPromises).then(function() {
-        setup.checkForUpdates().then(function() {
-          var today = new Date();
-          setup.get("settings").set("lastUpdatedDate", today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate());
-          setup.get("profilerDataUtils").marshalSavedAnswers();
-          resolve();
+          var staticPromises = {
+            questions: setup.staticQuestions(),
+            questionOptions: setup.staticQuestionOptions(),
+            clusters: setup.staticClusters(),
+            pathways: setup.staticPathways(),
+            careers: setup.staticOnetCareers(),
+            occupations: setup.staticOccupations(),
+            alumni: setup.staticAlumni(),
+            programs: setup.staticPrograms()
+          };
+
+          Ember.RSVP.hash(staticPromises).then(function() {
+            localforage.setItem(setup.localForageKey, setup.localForageData);
+            setup.checkForUpdates().then(function() {
+              var today = new Date();
+              setup.get("settings").set("lastUpdatedDate", today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate());
+              setup.get("profilerDataUtils").marshalSavedAnswers().then(function(updated) {
+                resolve();
+              });
+            });
+          });
+
         });
       });
+
     });
   }
 
