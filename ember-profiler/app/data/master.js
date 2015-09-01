3bf21ecab0bfb5240a77db6c353e529a1d85e16f
Toby Lounsbury <toby@ginkgostreet.com>
2015-09-01 11:57:29 -0700

    Added Master length table

---
+++ ember-profiler/app/data/master.js
@@ -0,0 +1,12 @@
+// jshint ignore: start
+var staticMasterData = {
+  alumni: 2,
+  clusters: 22,
+  occupations: 643,
+  onetCareers: 1085,
+  pathways: 77,
+  programs: 13798,
+  questions: 60,
+  questionOptions: 5
+};
+export default staticMasterData;
