d5b78374fff17ecebb34d0cecaa81fcbade4c23a
Toby Lounsbury <toby@ginkgostreet.com>
2015-09-02 23:12:09 -0700

    Updated view pathways to take into account lazy loading and model changes

--- ember-profiler/app/controllers/pathway.js
+++ ember-profiler/app/controllers/pathway.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  occupations: function() {
+    return this.get("model").occupations;
+  }.property('model'),
   actions: {
     exploreColleges: function() {
       this.modal.alert("This has not yet been implemented");
