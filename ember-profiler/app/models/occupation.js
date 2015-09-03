9794783be8cb0302831b84c55264362af489cc56
Toby Lounsbury <toby@ginkgostreet.com>
2015-09-02 23:12:44 -0700

    Updated model to take into account lazy loading

--- ember-profiler/app/models/occupation.js
+++ ember-profiler/app/models/occupation.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  pathway: DS.belongsTo('pathway'),
-  name: DS.attr('string'),
+  pathway: DS.belongsTo('pathway', { async: true }),
+  title: DS.attr('string'),
   score: DS.attr('number', {defaultValue: 0}),
   awardDescription: DS.attr('string'),
   totalAwards: DS.attr('number', {defaultValue: 0}),
