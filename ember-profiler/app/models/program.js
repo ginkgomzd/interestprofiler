24c70ad89a8196528d0b07013846b4d59c84f9d6
Toby Lounsbury <toby@ginkgostreet.com>
2015-09-22 12:25:04 -0700

    Fixed misnamed model fields

--- ember-profiler/app/models/program.js
+++ ember-profiler/app/models/program.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  occupation: DS.hasMany('occupation'),
+  //occupation: DS.hasMany('occupation', { async: true }),
   collegeName: DS.attr('string'),
   collegeUrl: DS.attr('string'),
   title: DS.attr('string'),
