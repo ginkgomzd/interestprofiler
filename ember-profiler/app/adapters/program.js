f9ad52b690cdd30308d8aee2c0dba75c89660211
Toby Lounsbury <toby@ginkgostreet.com>
2015-09-22 12:20:17 -0700

    Overrode query function for program search

--- ember-profiler/app/adapters/program.js
+++ ember-profiler/app/adapters/program.js
@@ -1,5 +1,28 @@
 import LFAdapter from 'ember-localforage-adapter/adapters/localforage';
 
 export default LFAdapter.extend({
-  namespace: 'H2CPrograms'
+  namespace: 'H2CPrograms',
+  query: function (records, query) {
+    console.log("My Query Function! And my bubbles!");
+    if(typeof(query) === "object" && query.hasOwnProperty("occupation")) {
+      var results = [],
+        id, record, property, test, push;
+      for (id in records) {
+
+        record = records[id];
+        push = false;
+        if(records[id].hasOwnProperty("occupation")) {
+            push = records[id].occupation.indexOf(query.occupation) !== -1;
+        }
+
+        if (push) {
+          results.push(record);
+        }
+
+      }
+      return results;
+    } else {
+      return this._super(records, query);
+    }
+  }
 });
