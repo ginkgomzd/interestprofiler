642d3f5f9d37be317bd426fc72b419820c6d57af
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-10-17 03:50:19 -0700

    Changed keyname

--- ember-profiler/app/models/college.js
+++ ember-profiler/app/models/college.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   collegeName: DS.attr('string'),
-  collegeUrl: DS.attr('string'),
+  collegeURL: DS.attr('string'),
   address1: DS.attr('string'),
   address2: DS.attr('string'),
   city: DS.attr('string'),
