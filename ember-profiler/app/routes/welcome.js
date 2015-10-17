520b7b37494a1dd9a536d872281787693f772234
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-10-17 00:22:40 -0700

    CCC-135: Updated welcome screen per mockup

--- ember-profiler/app/routes/welcome.js
+++ ember-profiler/app/routes/welcome.js
@@ -3,7 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     takeQuiz: function() {
-      this.transitionTo("question", 1);
-    }
+      this.transitionTo("question", 0);
+    },
+    salaries: function() {
+      this.status.warn("This has not been implemented yet");
+    },
+    proximity: function() {
+      this.transitionTo("proximity");
+    },
   }
 });
