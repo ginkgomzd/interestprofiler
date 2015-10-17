e948dc436e29377e01be75d6581be7b47c4a0ccb
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-10-17 01:41:32 -0700

    Fixed parse error

--- ember-profiler/app/models/zipcode.js
+++ ember-profiler/app/models/zipcode.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  city: DS.attr('string')
-  lat: DS.attr('number')
+  city: DS.attr('string'),
+  lat: DS.attr('number'),
   long: DS.attr('number')
 });
