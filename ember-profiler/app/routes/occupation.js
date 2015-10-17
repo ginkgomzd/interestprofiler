e86af3777d62cf51c3557174c9c0dfb6589a5db3
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-10-17 06:55:18 -0700

    Connected occupation details screen to raw-data source

--- ember-profiler/app/routes/occupation.js
+++ ember-profiler/app/routes/occupation.js
@@ -1,10 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  rawData: Ember.inject.service('raw-data'),
   model: function(params) {
-    return {
+    return Ember.RSVP.hash({
       "occupation": this.store.find('occupation', params.index),
       "programs": this.store.find('program', {"occupation": params.index}),
-    };
+      "colleges": this.get("rawData").fetch("H2CColleges", 'college')
+    });
+  },
+  actions: {
+    logColleges: function(colleges) {
+      console.log(colleges);
+    }
   }
 });
