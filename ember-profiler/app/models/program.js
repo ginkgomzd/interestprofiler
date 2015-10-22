a0d0b5cf194e8590ebb90ebb6c94aa9eec93c509
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-10-21 20:30:44 -0700

    Removed old commented out fields from program model

--- ember-profiler/app/models/program.js
+++ ember-profiler/app/models/program.js
@@ -1,10 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  //occupation: DS.hasMany('occupation', { async: true }),
-  //occupation: DS.hasMany('occupation', { async: true }),
-  //collegeName: DS.attr('string'),
-  //collegeUrl: DS.attr('string'),
   college: DS.attr('number'),
   title: DS.attr('string'),
   awardDescription: DS.attr('string')
