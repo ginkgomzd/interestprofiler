d16bd3f354b10e75f0b66537c4ad3e1c34a233c9
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-10-23 23:35:41 -0700

    Removed unneeded pathway search

--- ember-profiler/app/routes/select-pathways.js
+++ ember-profiler/app/routes/select-pathways.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function () {
     return Ember.RSVP.hash({
-      clusters: this.get('store').find('cluster', {is_selected: true}),
-      allPathways: this.get('store').find('pathway')
+      clusters: this.get('store').find('cluster', {is_selected: true})//,
+      //allPathways: this.get('store').find('pathway')
     });
   }
 });
