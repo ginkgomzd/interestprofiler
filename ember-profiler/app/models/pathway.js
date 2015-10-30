b6e629f6d59d52ebd2cfc6456f7865cd3f4a72e1
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-10-29 20:28:21 -0700

    Added description to pathway model

--- ember-profiler/app/models/pathway.js
+++ ember-profiler/app/models/pathway.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
+  description: DS.attr('string'),
   cluster: DS.belongsTo('cluster', { async: true }),
   occupations: DS.hasMany('occupation', { async: true }),
   onetCareers: DS.hasMany('onet-career', { async: true }),
