32e4482ac7bf8f15b8b0181b0605c9fa89156e46
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-10-31 19:01:42 -0700

    added pathway to alumni model

--- ember-profiler/app/models/alumni.js
+++ ember-profiler/app/models/alumni.js
@@ -4,7 +4,7 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   desc: DS.attr('string'),
   img: DS.attr('string'),
-
+  pathway: DS.belongsTo('pathway', { async: true }),
   getImgPath: function() {
     if(this.get("img").search("http:") !== -1) {
       return this.get('img');
