628c80ecf97bebe152bfba28891453a3e11a5c22
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-11-09 15:31:54 -0800

    Added ignoredTargets list to panRight so it doesn't break slider opperation

--- ember-profiler/app/views/application.js
+++ ember-profiler/app/views/application.js
@@ -8,6 +8,10 @@ export default Ember.View.extend({
     this.get("controller").set("drawerSwipeEnabled", true);
   },
   panRight: function(e) {
+    var ignoredTargets = ["ProximitySlider"];
+    if(ignoredTargets.indexOf(e.target.id) !== -1) {
+      return e;
+    }
     if (this.get("controller").get("drawerSwipeEnabled")) {
       if (e.originalEvent.gesture.deltaX > 100) {
         if (e.originalEvent.gesture.pointers[0].pageX < 150) {
