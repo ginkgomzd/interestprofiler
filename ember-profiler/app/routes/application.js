a0e749978b66b66166384eaad3b030d78319dade
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-11-11 18:14:13 -0800

    Revert "Forced app to load to welcome screen after login"
    This reverts commit 313c978d5f3febbd611f48f5a6361adc33362f38.

--- ember-profiler/app/routes/application.js
+++ ember-profiler/app/routes/application.js
@@ -11,8 +11,6 @@ export default Ember.Route.extend({
   beforeModel: function() {
     if(!this.parseAuth.loggedIn) {
       this.transitionTo("login");
-    } else {
-      this.transitionTo("welcome");
     }
   },
   actions: {
