a5a85157b68ed144184b9328ace039fb844e38ee
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-11-11 19:00:33 -0800

    Updated select-pathways to use new settings based cluster selection

--- ember-profiler/app/routes/select-pathways.js
+++ ember-profiler/app/routes/select-pathways.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function () {
     return Ember.RSVP.hash({
-      clusters: this.get('store').find('cluster', {is_selected: true})//,
+      clusters: this.get('store').find('cluster', {"in": this.get("settings").load("selectedClusters")})
       //allPathways: this.get('store').find('pathway')
     });
   }
