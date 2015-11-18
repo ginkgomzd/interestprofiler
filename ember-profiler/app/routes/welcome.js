8722cf290407d69623af5ba738190beeb8750dfb
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-11-17 16:52:04 -0800

    Added welcome screen selection analytics

--- ember-profiler/app/routes/welcome.js
+++ ember-profiler/app/routes/welcome.js
@@ -3,12 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     takeQuiz: function() {
+      this.send("analytics", "welcomeSelection", "My Career Interests");
       this.transitionTo("question", 0);
     },
     salaries: function() {
+      this.send("analytics", "welcomeSelection", "Inland Empire Salaries");
       this.transitionTo("select-clusters");
     },
     proximity: function() {
+      this.send("analytics", "welcomeSelection", "Local Degrees and Colleges");
       this.transitionTo("proximity");
     }
   }
