47ff0a3b1c2e96db85e03ad17474c2377ffff81b
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-12-15 10:31:54 -0800

    Added wage data grouped by Occupation

--- ember-profiler/app/routes/cluster.js
+++ ember-profiler/app/routes/cluster.js
@@ -11,13 +11,31 @@ export default Ember.Route.extend({
 
           pathways.forEach( function(item) {
             promises.push(that.store.find("occupation", {"pathway": item.id}).then(function(occupations) {
-              //console.log(occupations.toArray());
-              occupationList = occupationList.concat( occupations.toArray() );
+              occupations = occupations.toArray();
+              var i = 0;
+              while(i < occupations.length) {
+                var rowHasData = true;
+                if(occupations.hasOwnProperty(i)) {
+                  if (occupations.hasOwnProperty(i + 1)) {
+                    if (occupations[i].get("title") ===  occupations[i + 1].get("title")) {
+                      rowHasData = (occupations[i].get("hasWageData") || occupations[i + 1].get("hasWageData"));
+                      occupationList.push({first: occupations[i], second: occupations[i + 1], hasData: rowHasData});
+                      i++;
+                    } else {
+                      rowHasData = occupations[i].get("hasWageData");
+                      occupationList.push({first: occupations[i], hasData: rowHasData});
+                    }
+                  } else {
+                    rowHasData = occupations[i].get("hasWageData");
+                    occupationList.push({first: occupations[i], hasData: rowHasData});
+                  }
+                }
+                i++;
+              }
             }));
           });
 
           Ember.RSVP.all(promises).then(function() {
-            console.log(occupationList);
             cluster.occupations = occupationList;
             resolve(cluster);
           });
