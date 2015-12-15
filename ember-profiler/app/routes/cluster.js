970e46f9e392dc5c2012996160643f8230a6385f
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-12-14 19:34:58 -0800

    Updated model function to fetch occupations

--- ember-profiler/app/routes/cluster.js
+++ ember-profiler/app/routes/cluster.js
@@ -2,6 +2,25 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    return this.store.find('cluster', params.index);
+    var that = this;
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      that.get("store").find('cluster', params.index).then(function(cluster) {
+        cluster.get("pathways").then(function(pathways) {
+          var promises = [];
+          var occupationList = [];
+
+          pathways.forEach( function(item) {
+            promises.push(item.get("occupations").then(function(occupations) {
+              occupationList += occupations;
+            }));
+          });
+
+          Ember.RSVP.all(promises).then(function() {
+            cluster.occupations = occupationList;
+            resolve(cluster);
+          });
+        });
+      });
+    });
   }
 });
