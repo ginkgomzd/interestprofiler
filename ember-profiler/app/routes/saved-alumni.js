32a7448a4781e5cc2cb697654ee7c6bd36577db3
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2015-12-18 08:11:43 -0800

    Fetched data for saved alumni listing

--- ember-profiler/app/routes/saved-alumni.js
+++ ember-profiler/app/routes/saved-alumni.js
@@ -3,6 +3,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   rawData: Ember.inject.service('raw-data'),
   model: function (params) {
-
-  },
+    var that = this;
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      that.get("store").find("hotOrNot", {hot: true}).then(function(savedAlumni) {
+        var savedAlumniIds = [];
+        savedAlumni.forEach(function(item) {
+          savedAlumniIds.push(item.get("id"));
+        });
+        that.get("store").find("alumni", {in: savedAlumniIds}).then(function(alumni) {
+          resolve(alumni);
+        });
+      });
+    });
+  }
 });
