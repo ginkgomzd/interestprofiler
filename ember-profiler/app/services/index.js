b1b978e9ec76eea447b1cb8639218d73ec4c4222
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-02-25 12:10:23 -0800

    Added new indexService

---
+++ ember-profiler/app/services/index.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+var indexService = Ember.Object.extend({
+  rawData: Ember.inject.service('raw-data'),
+  find: function(modelName, indexMapping, id, store) {
+    var rawData = this.get("rawData");
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      rawData.fetchIndex(indexMapping.emberDataNamespace, indexMapping.modelName, id).then(function(ids) {
+        if(ids.length > 0) {
+          store.find(modelName, {in: ids}).then(function (models) {
+            resolve(models);
+          }, function(error) {
+            reject(error);
+          });
+        } else {
+          resolve([]);
+        }
+      });
+    });
+  }
+});
+export default indexService;
