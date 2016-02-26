7bcd667ec3e84fa9d1c08d2ab3e624a8d27e12bd
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-02-26 05:25:48 -0800

    Added and modified controllers to reflect back button hide/show spec

--- ember-profiler/app/controllers/proximity.js
+++ ember-profiler/app/controllers/proximity.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  showBackButton: "ios",
   searchRadius: function() { return 25;}.property(),
   deferredUpdate: function() {this.send("findInProximity");},
   liveUpdateProximity: function() {
