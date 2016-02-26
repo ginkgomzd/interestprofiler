cfcbca94273c03d9f0291cf1b8a17188eaf61d7f
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-02-26 07:47:32 -0800

    Refactored model function of results screen so the spinner blocks only when necessary, and added appropriate error messages

--- ember-profiler/app/routes/results.js
+++ ember-profiler/app/routes/results.js
@@ -5,7 +5,19 @@ export default Ember.Route.extend({
     model: function() {
 
       if (this.get("profilerDataUtils").dirtyAnswers()) {
-        return this.get("profilerDataUtils").updateAllResults('results');
+        var that = this;
+        return new Ember.RSVP.Promise(function(resolve, reject) {
+          that.get("profilerDataUtils").updateAllResults().then(function() {
+            that.store.findAll('scoreArea').then(function(models) {
+              resolve(models);
+            });
+          }, function() {
+            that.get("status").error("We were unable to fetch your results at this time", "Network Error");
+            that.store.findAll('scoreArea').then(function(models) {
+              resolve(models);
+            });
+          });
+        });
       }
 
       return this.store.find('scoreArea');
