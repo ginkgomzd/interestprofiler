35d72e6a335f229243b86a5ae886c60d7e4ae11d
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-03-31 10:59:28 -0700

    Refactored login screen per client request, cleaned up layout, to make it more maintainable, moved ui actions to controller

--- ember-profiler/app/routes/login.js
+++ ember-profiler/app/routes/login.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   setupUtils: Ember.inject.service('setup'),
+  status: Ember.inject.service('status'),
+  dateHelper: Ember.inject.service('date-functions'),
   beforeModel: function() {
     if (this.parseAuth.loggedIn) {
       this.transitionTo("welcome");
@@ -15,11 +17,31 @@ export default Ember.Route.extend({
       });
     }
   },
+  verifyAge: function() {
+    var valid = Ember.$("#ageVerificationCheckbox").is(":checked");
+    if(!valid) {
+      this.get("status").warn("You must be at least 13 years of age to create an account.");
+    }
+    return valid;
+  },
+  saveTosAndAgeVerification: function() {
+    var today = this.get("dateHelper").formatDateTime();
+    this.get("settings").save("ageVerified", today);
+    this.get("settings").save("tosVerified", today);
+  },
   actions: {
-    loginWithFacebook: function() {
+    signupWithFacebook: function() {
+      if (this.verifyAge()) {
+        this.send("loginWithFacebook", true);
+      }
+    },
+    loginWithFacebook: function(saveVerification) {
       var that = this;
       this.parseAuth.authenticate_fb(function() {
         that.registerLoginLocationAnalytics("facebook");
+        if(saveVerification) {
+          that.saveTosAndAgeVerification();
+        }
         that.transitionTo("welcome");
       },
       function(user, error) {
@@ -30,7 +52,15 @@ export default Ember.Route.extend({
     loginWithLinkedIn: function() {this.status.warn("This hasn't been implemented yet");},
     loginWithTwitter: function() {this.status.warn("This hasn't been implemented yet");},
     loginWithGoogle: function() {this.status.warn("This hasn't been implemented yet");},
-    doLogin: function() {
+
+    /**
+     * Function for age validation
+     */
+
+
+
+    /** Email function **/
+    doEmailLogin: function() {
       var that = this;
       var user = {
         email: Ember.$("#login-email").val(),
@@ -48,36 +78,39 @@ export default Ember.Route.extend({
           that.status.warn(error.message);
         });
     },
-    doSignup: function() {
-      var user = {
-        email: Ember.$("#login-email").val(),
-        password: Ember.$("#login-password").val()
-      };
-      if (Ember.$("#login-password-confirm").val() === user.password) {
-        var that = this;
-        this.parseAuth.register(user,
-          function() {
-            //success
-              that.get("setupUtils").handleLogin().then(function() {
+    doEmailSignup: function() {
+      if (this.verifyAge()) {
+        var user = {
+          email: Ember.$("#signup-email").val(),
+          password: Ember.$("#signup-password").val()
+        };
+        if (Ember.$("#signup-password-confirm").val() === user.password) {
+          var that = this;
+          this.parseAuth.register(user,
+            function () {
+              //success
+              that.get("setupUtils").handleLogin().then(function () {
                 that.registerLoginLocationAnalytics("email-signup");
+                that.saveTosAndAgeVerification();
                 that.transitionTo("welcome");
+              });
+            },
+            function (user, error) {
+              that.status.warn(error.message);
             });
-          },
-          function(user, error) {
-            that.status.warn(error.message);
-          });
-      } else {
-        this.status.warn("Passwords don't match");
+        } else {
+          this.status.warn("Passwords don't match");
+        }
       }
     },
-    doReset: function() {
-      if (Ember.$("#login-email").val()) {
+    doEmailReset: function() {
+      if (Ember.$("#reset-email").val()) {
 
         var that = this;
-        this.parseAuth.passwordReset(Ember.$("#login-email").val(), {
+        this.parseAuth.passwordReset(Ember.$("#reset-email").val(), {
           success: function () {
-            that.status.success("An email with reset instructions has been sent to '" + Ember.$("#login-email").val() + "'");
-            that.send("cancelReset");
+            that.status.success("An email with reset instructions has been sent to '" + Ember.$("#reset-email").val() + "'");
+            that.controller.send("showLoginEmail");
           },
           error: function (error) {
             that.status.error(error.message);
@@ -86,25 +119,6 @@ export default Ember.Route.extend({
       } else {
         this.status.warn("You must supply your email address to reset your password.");
       }
-    },
-    showReset: function() {
-      Ember.$("#login-actions, #login-password").slideUp();
-      Ember.$("#reset-actions").slideDown();
-    },
-    cancelReset: function() {
-      Ember.$("#login-actions, #login-password").slideDown();
-      Ember.$("#reset-actions").slideUp();
-      Ember.$("#login-password").css("display", "block");
-    },
-    showSignup: function() {
-      Ember.$("#login-actions, #reset-actions").slideUp();
-      Ember.$("").slideDown();
-      Ember.$("#signup-actions, #login-password-confirm").slideDown();
-      Ember.$("#login-password-confirm").css("display", "block");
-    },
-    cancelSignup: function() {
-      Ember.$("#login-actions").slideDown();
-      Ember.$("#signup-actions, #login-password-confirm").slideUp();
-    },
+    }
   }
 });
