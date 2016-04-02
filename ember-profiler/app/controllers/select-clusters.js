66bc6134d4ca46d2ec4830a6b2293c1bffd450c5
TobiasLounsbury <tobiaslounsbury@gmail.com>
2016-04-01 23:27:42 -0700

    Added show back button to program listing

--- ember-profiler/app/controllers/select-clusters.js
+++ ember-profiler/app/controllers/select-clusters.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend(Ember.SortableMixin, {
   pageTitle: "Wages and Salaries",
   navbarClass: "lightBlue",
+  showBackButton: "ios",
   sortProperties: ['score', 'jobGrowth', 'salaryGrowth'],
   sortAscending: false,
   actions: {
