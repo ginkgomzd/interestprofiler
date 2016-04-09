bcd42bee7ffa4670de47bf53f15ac9ca560afd08
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-04-08 20:47:02 -0700

    Updated data structures

--- ember-profiler/app/routes/demo.js
+++ ember-profiler/app/routes/demo.js
@@ -3,10 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     return [
-      "Slide1.png",
-      "Slide2.png",
-      "Slide3.png",
-      "Slide4.png"
+      {img: "Slide1.png", text1: "Welcome to Here to Career!", text2: "Thanks, let's go!"},
+      {img: "Slide2.png", text1: "Check Wages &amp; Salaries<br />for local careers.", text2: ""},
+      {img: "Slide3.png", text1: "Take the Jobs &amp;Careers<br />quiz to find out which<br />career is right for you.", text2: ""},
+      {img: "Slide4.png", text1: "Match with<br />California Community<br />College Alumni.", text2: ""},
+      {img: "Slide5.png", text1: "Search for nearby<br />Degrees &amp; Colleges.", text2: ""}
     ];
   },
   actions: {
