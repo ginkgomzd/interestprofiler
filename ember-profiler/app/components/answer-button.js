c593c97d3c15258792b2250ae53962505adb4467
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-06-15 12:08:06 -0700

    Updated answer-button to properly handle clicks

--- ember-profiler/app/components/answer-button.js
+++ ember-profiler/app/components/answer-button.js
@@ -1,22 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'button',
+  tagName: 'input',
   classNames : ["button"],
+  attributeBindings: ["opt.label:value", "type"],
+  type: "button",
   classNameBindings : ["answerIndex"],
   answerIndex: function() { return "answer" + this.opt.get("value");}.property(),
-  templateName: 'answer',
-  tap: function() {
-    this.makeSelection(this.opt.get("value"));
-  },
-  press: function() {
+  click: function() {
     this.makeSelection(this.opt.get("value"));
   },
   makeSelection: function(selectedAnswer) {
     var that = this;
     this.$().animate({backgroundColor: Ember.$.Color({ alpha: 0.3 })}, 200, function() {
       that.$().animate({backgroundColor: Ember.$.Color({ alpha: 0 })}, 200, function() {
-        that.sendAction("click", selectedAnswer);
+        that.sendAction("onClick", selectedAnswer);
       });
     });
   },
