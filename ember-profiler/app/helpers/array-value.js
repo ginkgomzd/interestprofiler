3fe56bc187cbabff9c42af1bcf92076ac963d7fe
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-06-15 13:10:27 -0700

    Updated array-value helper to 1.13.x+ version which should work for 2.4, though it doesn't appear to be used currently

--- ember-profiler/app/helpers/array-value.js
+++ ember-profiler/app/helpers/array-value.js
@@ -1,6 +1,6 @@
 import Ember from "ember";
 
-/* This works for 1.13.x
+
 export default Ember.Helper.helper(function(params) {
   if (params[0].hasOwnProperty(params[1]) && params[0][params[1]].hasOwnProperty(params[2])) {
     return params[0][params[1]][params[2]];
@@ -8,8 +8,8 @@ export default Ember.Helper.helper(function(params) {
     return "";
   }
 });
-*/
 
+/* This is the old format, I'm leaving it here for reference
 export default Ember.Handlebars.makeBoundHelper( function(data, key, field) {
   if (data.hasOwnProperty(key) && data[key].hasOwnProperty(field)) {
     return data[key][field];
@@ -17,3 +17,4 @@ export default Ember.Handlebars.makeBoundHelper( function(data, key, field) {
     return "";
   }
 });
+  */
