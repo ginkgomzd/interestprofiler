37db7a0a42e5baf10287b0c2e4c60b56b95a3719
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-06-17 16:00:36 -0700

    Refactored settings screen

--- ember-profiler/app/controllers/settings.js
+++ ember-profiler/app/controllers/settings.js
@@ -2,29 +2,37 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   status: Ember.inject.service('status'),
+  parseAuth: Ember.inject.service('parse-auth'),
   attributes: ["email", "firstName", "lastName", "age", "location", "education"],
   user: function() {
     var user = {};
-    for (var i in this.attributes) {
-      user[this.attributes[i]] = this.parseAuth.user.get(this.attributes[i]);
+    if (this.get("parseAuth").loggedIn) {
+      for (var i in this.attributes) {
+        user[this.attributes[i]] = this.get("parseAuth").user.get(this.attributes[i]);
+      }
     }
     return user;
   }.property("parseAuth.user"),
   actions: {
     saveUser: function() {
-      var user = this.get("user");
-      for (var i in this.attributes) {
-        this.parseAuth.user.set(this.attributes[i], user[this.attributes[i]]);
-      }
-      var that = this;
-      this.parseAuth.user.save(null, {
-        success: function() {
-          that.get("status").success("Changes Saved");
-        },
-        error: function(error) {
-          that.get("status").error(error.message);
+      if (this.get("parseAuth").loggedIn) {
+        var user = this.get("user");
+        for (var i in this.attributes) {
+          this.get("parseAuth").user.set(this.attributes[i], user[this.attributes[i]]);
         }
-      });
+        var that = this;
+        this.get("parseAuth").user.save(null, {
+          success: function () {
+            that.get("status").success("Changes Saved");
+          },
+          error: function (error) {
+            that.get("status").error(error.message);
+          }
+        });
+      }
+    },
+    gotoLogin: function() {
+      this.transitionToRoute("login");
     }
   }
 });
