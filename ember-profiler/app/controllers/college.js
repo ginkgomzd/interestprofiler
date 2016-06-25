ea84640f705ed6bfcddc4906f22a5d321f689df0
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-06-24 22:09:53 -0700

    CCC-321: Added action for adding events to phone calendar

--- ember-profiler/app/controllers/college.js
+++ ember-profiler/app/controllers/college.js
@@ -1,6 +1,35 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  status: Ember.inject.service('status'),
   showBackButton: "ios",
-  pageTitle: "Degrees and Colleges"
+  pageTitle: "Degrees and Colleges",
+  actions: {
+    addToCalendar: function(date) {
+
+      if(window.plugins && window.plugins.calendar) {
+        var dateObj = new Date(date.date.value);
+        var calOptions = window.plugins.calendar.getCalendarOptions();
+        var that = this;
+        //The params order:
+        //title,eventLocation,notes,startDate,endDate,calOptions,success,error
+        window.plugins.calendar.createEventInteractivelyWithOptions(
+          date.title,
+          null,
+          date.description,
+          dateObj,
+          dateObj,
+          calOptions,
+          function(msg) { //Success
+            that.get("status").success("Event added successfully");
+          },
+          function(msg) { //Error
+            that.get("status").warn("Failed to add this event to your calendar");
+          });
+
+      } else {
+        console.log("Cannot add the Date");
+      }
+    }
+  }
 });
