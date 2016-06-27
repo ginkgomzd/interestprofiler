40061600e66986cf851874a7f6603b9b08d621ed
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-06-27 14:47:30 -0700

    Added calls to lock the screen orientation on demo-load and unlock it after the demo has been completed

--- ember-profiler/app/routes/demo.js
+++ ember-profiler/app/routes/demo.js
@@ -3,6 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   settings: Ember.inject.service('settings'),
   model: function(params) {
+    //Lock screen orientation into portrait.
+    try {
+      window.screen.lockOrientation("portrait");
+    } catch(e) {}
+
+    //Return the data for the Demo
     return [
       {img: "Slide1.png", text1: "Welcome to Here to Career!", text2: "Thanks, let's go!"},
       {img: "Slide2.png", text1: "Check Wages &amp; Salaries<br />for local careers.", text2: ""},
@@ -11,8 +17,13 @@ export default Ember.Route.extend({
       {img: "Slide5.png", text1: "Search for nearby<br />Degrees &amp; Colleges.", text2: ""}
     ];
   },
+
   actions: {
     navigateAway: function() {
+      try {
+        window.screen.unlockOrientation();
+      } catch(e) {}
+
       this.get("settings").save("demoSeen", true, true);
       this.transitionTo("welcome");
     }
